da6940d2865e741eab12388fe2e74e79f8908646
grisha <norsulfasol@gmail.com>
2023-01-10 17:34:51 +0300

    ДЗ 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         // Задание 1
         Human maksim = new Human(1988, "Максим", "Минск","Бренд-менеджер");
@@ -16,35 +17,6 @@ public class Main {
         System.out.println(anna);
         System.out.println(maksim);
         System.out.println(vladimir);
-
-
-        //ООП. Урок 2 (инкапсуляция) Домашнее задание 1 Задание 2 "Создайте новый класс flower."
-
-        Flower rose = new Flower("", "Голландия", 35.59,3,"Роза");
-        Flower chrysanthemum = new Flower("", "", 15.0, 5,"Хризантема");
-        Flower pion = new Flower("", "Англия", 69.9, 1,"Пион");
-        Flower gypsophila = new Flower("", "Турция", 19.50, 10,"Гипсофила");
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-//        rose.setFlowerColor("  ");
-//        chrysanthemum.setFlowerColor(null);
-//        chrysanthemum.setCountry("");
-//        pion.setFlowerColor("    "+"   ");
-//        pion.setCost(-25);
-//        gypsophila.setFlowerColor("желтый");
-//        gypsophila.setCountry("     ");
-        System.out.println(rose);
-        System.out.println(chrysanthemum);
-        System.out.println(pion);
-        System.out.println(gypsophila);
-
-        Bouquet bouquet = new Bouquet();
-        bouquet.addFlower(rose).addFlower(rose).addFlower(rose).addFlower(gypsophila).addFlower(chrysanthemum).
-        addFlower(chrysanthemum).addFlower(chrysanthemum).addFlower(chrysanthemum).addFlower(chrysanthemum);
-        System.out.println(bouquet.getCost());
-        System.out.println(bouquet.getLifeSpan());
-
-
-
 
 
     }
